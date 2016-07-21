c6c8ea49fb0b3392ebbd5dd4cff41e88593b47d7
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-07-21 01:38:26 -0400

    Increment version to 0.1.1.

--- fgo.rb
+++ fgo.rb
@@ -1,14 +1,14 @@
 class Fgo < Formula
   desc "Build, upload, and create brew formula for golang application."
   homepage "https://github.com/jkawamoto/fgo"
-  version "0.1.0"
+  version "0.1.1"
 
   if Hardware.is_64_bit?
-    url "https://github.com/jkawamoto/fgo/releases/download/0.1.0/fgo_0.1.0_darwin_amd64.zip"
-    sha256 "ef2a9a2fb4e64a3a8c1e37d13ad43cb470d4e1a8adf3aa02537ace05633dc743"
+    url "https://github.com/jkawamoto/fgo/releases/download/0.1.1/fgo_0.1.1_darwin_amd64.zip"
+    sha256 "c562bde4c78fffd7729420748a1dd423205b73d74568fe3e8964f821d93bc566"
   else
-    url "https://github.com/jkawamoto/fgo/releases/download/0.1.0/fgo_0.1.0_darwin_386.zip"
-    sha256 "7a1d998f20d1e1e56ce11260480ba90d700ae4b427a9951309a82e1305d72931"
+    url "https://github.com/jkawamoto/fgo/releases/download/0.1.1/fgo_0.1.1_darwin_386.zip"
+    sha256 "52c32075e00d8c7cdecb88763c70e8449b4adf4461bc1bf8bbc1a6beb649641c"
   end
 
   def install
