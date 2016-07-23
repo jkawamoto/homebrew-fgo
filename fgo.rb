a39a299b7eb0e9abe51d51b865c6229873dbcf24
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-07-23 02:09:09 -0400

    Version 0.2.2.

--- fgo.rb
+++ fgo.rb
@@ -1,14 +1,14 @@
 class Fgo < Formula
   desc "Build, upload, and create brew formula for golang application."
   homepage "https://github.com/jkawamoto/fgo"
-  version "0.2.0"
+  version "0.2.2"
 
   if Hardware.is_64_bit?
-    url "https://github.com/jkawamoto/fgo/releases/download/0.2.0/fgo_0.2.0_darwin_amd64.zip"
-    sha256 "c63cbd7ac9240d674a64ebd80ea9067cec57ecc919e7fb1e32a6b6b3ce5fd8b0"
+    url "https://github.com/jkawamoto/fgo/releases/download/0.2.2/fgo_0.2.2_darwin_amd64.zip"
+    sha256 "756e2d9edbd8f1c81e30eb8fba3f08d8cbbc409da28914de77f63762575fde33"
   else
-    url "https://github.com/jkawamoto/fgo/releases/download/0.2.0/fgo_0.2.0_darwin_386.zip"
-    sha256 "75f282b5a54702788598ecd47d9881d8c7925145194b29d3621e94be6b14dfcf"
+    url "https://github.com/jkawamoto/fgo/releases/download/0.2.2/fgo_0.2.2_darwin_386.zip"
+    sha256 "088f6283421b1e8aa223f6d075721e020d95a5b22b9333c408ac216a3c4c8b59"
   end
 
   def install
