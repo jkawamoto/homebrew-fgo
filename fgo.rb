6c7ce5bb2e7047fde672897e4e92250e9071e04a
Travis CI User <travis@example.org>
2018-07-03 01:17:39 +0000

    v0.3.2

--- fgo.rb
+++ fgo.rb
@@ -11,18 +11,18 @@ require 'rbconfig'
 class Fgo < Formula
   desc "Build, upload, and create brew formula for golang application."
   homepage "https://github.com/jkawamoto/fgo"
-  version "0.3.1"
+  version "v0.3.2"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jkawamoto/fgo/releases/download/v0.3.1/fgo_0.3.1_darwin_amd64.zip"
-      sha256 "217c78ba31b2ff535b92d71e8dac27380f615165fbaa7a88e23ef3ca8519f3c2"
+      url "https://github.com/jkawamoto/fgo/releases/download/vv0.3.2/fgo_v0.3.2_darwin_amd64.zip"
+      sha256 "57b9ba097e800ab0e07521f83d31a33cf6b29db537e1e36508a5efdec645cedb"
     when /linux/
-      url "https://github.com/jkawamoto/fgo/releases/download/v0.3.1/fgo_0.3.1_linux_amd64.tar.gz"
-      sha256 "70b07aefc35caa20b3f444bc70f4c4ee96b44e04cac66f3adb3edc3f1ff443a2"
+      url "https://github.com/jkawamoto/fgo/releases/download/vv0.3.2/fgo_v0.3.2_linux_amd64.tar.gz"
+      sha256 "4c56ecd3eb9d5a009703f5ace67e9804bade937302abb69dee8437a5000b85c8"
     when /solaris|bsd/
       :unix
     else
@@ -33,11 +33,11 @@ class Fgo < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jkawamoto/fgo/releases/download/v0.3.1/fgo_0.3.1_darwin_386.zip"
-      sha256 "3746a7a21409f719f7286c8e4e83b21ada5c6e7e2a4989c3dab6ee628311c046"
+      url "https://github.com/jkawamoto/fgo/releases/download/vv0.3.2/fgo_v0.3.2_darwin_386.zip"
+      sha256 "f9e1bf2f53071434b06c84668118df26f53df4c3500c7cdae4fe31060b542678"
     when /linux/
-      url "https://github.com/jkawamoto/fgo/releases/download/v0.3.1/fgo_0.3.1_linux_386.tar.gz"
-      sha256 "0709798bf4468feb564b1332a38c7e3fe97a8aa1fd3ef737c98de2ceeb04ae5d"
+      url "https://github.com/jkawamoto/fgo/releases/download/vv0.3.2/fgo_v0.3.2_linux_386.tar.gz"
+      sha256 "a17906d685d0e0b386d51bfd2ecf778175e64fbdd7a88dd7e2c45a230bc535d6"
     when /solaris|bsd/
       :unix
     else
